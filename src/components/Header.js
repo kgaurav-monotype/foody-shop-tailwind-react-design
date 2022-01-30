03da84be9bd375944b1b2d29decaad05af1bb63a
kumar247001 <kumar247001@gmail.com>
2022-01-30 16:41:46 +0530

    update readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,31 +26,22 @@ const Header = () => {
                     </div>
                     <div className={`${!mobile ? "md:block" : "hidden"} ${active ? "md:block" : "hidden"} transition ease-linear duration-500`}>
                     <ul className="flex flex-col">
-                        <li className='border-l-4 border-gray-900 hover:border-red-600 mb-5 px-4'>
-                            <a href="#" className='flex'>
+                        <li className='flex border-l-4 border-gray-900 hover:border-red-600 mb-5 px-4'>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                 </svg>
                                 <span>Home</span>
-                            </a>
                         </li>
-                        <li className='border-l-4 border-gray-900 hover:border-red-600 mb-5 px-4'>
-                            <a href="#" className='flex'>
-
+                        <li className='flex border-l-4 border-gray-900 hover:border-red-600 mb-5 px-4'>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                                 <span>About</span>
-                            </a>
                         </li>
-                        <li className='border-l-4 border-gray-900 hover:border-red-600 mb-5 px-4'>
-                            <a href="#" className='flex'>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <li className='flex border-l-4 border-gray-900 hover:border-red-600 mb-5 px-4'><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
                                 <span>Contact</span>
-                            </a>
                         </li>
                     </ul>
                     <div className='flex justify-start sm:flex-col lg:flex-row p-4'>
